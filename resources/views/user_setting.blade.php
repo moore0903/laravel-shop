ebc5d5cc831c87bbb9d94d186c0e931b55c90498
yankai <moore0903@yeah.net>
2017-05-09 22:40:21 +0800

    优化代码

--- resources/views/user_setting.blade.php
+++ resources/views/user_setting.blade.php
@@ -3,7 +3,7 @@
 @section('content')
     <div class="wrap fmyh">
         <div class="land">
-            <p class="fhaniu"><a href="javascript:window.history.go(-1)"></a></p>
+            <p class="fhaniu"><a href="{{url('user/info')}}"></a></p>
             个人设置
         </div>
         <ul class="personal lifl clear">
