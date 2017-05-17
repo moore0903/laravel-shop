afeb06a44633a3ee5398c75013cbf351178b5b1a
yankai <moore0903@yeah.net>
2017-05-17 15:17:27 +0800

    新增秒杀方法

--- app/Models/SecKill.php
+++ app/Models/SecKill.php
@@ -15,4 +15,9 @@ class SecKill extends Model
     public function shopItem() {
         return $this->belongsTo(ShopItem::class,'shop_item_id');
     }
+
+    public static function getSecKill($page=5){
+        $time = date('Y-m-d h:i:s');
+        return SecKill::where('start_time','<',$time)->where('end_time','>',$time)->groupBy('shop_item_id')->take($page)->with('shopItem')->get();
+    }
 }
