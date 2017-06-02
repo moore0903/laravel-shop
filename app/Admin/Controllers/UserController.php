c0b2dd921abb4f8eecef0368b1d1c6d9f65d1449
yankai <moore0903@yeah.net>
2017-06-02 11:49:31 +0800

    新增会员列表,查看会员的收货地址

---
+++ app/Admin/Controllers/UserController.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\User;
+
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Facades\Admin;
+use Encore\Admin\Layout\Content;
+use App\Http\Controllers\Controller;
+use Encore\Admin\Controllers\ModelForm;
+use Encore\Admin\Widgets\Table;
+
+class UserController extends Controller
+{
+    use ModelForm;
+
+    /**
+     * Index interface.
+     *
+     * @return Content
+     */
+    public function index()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('会员列表');
+            $content->description('会员列表');
+
+            $content->body($this->grid());
+        });
+    }
+
+    /**
+     * Edit interface.
+     *
+     * @param $id
+     * @return Content
+     */
+    public function edit($id)
+    {
+        return Admin::content(function (Content $content) use ($id) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form()->edit($id));
+        });
+    }
+
+    /**
+     * Create interface.
+     *
+     * @return Content
+     */
+    public function create()
+    {
+        return Admin::content(function (Content $content) {
+
+            $content->header('header');
+            $content->description('description');
+
+            $content->body($this->form());
+        });
+    }
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        return Admin::grid(User::class, function (Grid $grid) {
+            $grid->model()->where('phone',\DB::raw('REGEXP'),\DB::raw('"[1][35678][0-9]{9}"'));
+            $grid->id('ID')->sortable();
+            $grid->phone('电话');
+
+            $grid->created_at('注册时间');
+
+            $grid->column('收货地址')->expand(function () {
+                $address = $this->address;
+                $address = $address->map(function($item){
+                    $data = ['realname'=>$item['realname'],'address'=>$item['address'],'phone'=>$item['phone']];
+                    foreach(array_diff_assoc($item->toArray(),$data) as $key => $value){
+                        unset($item[$key]);
+                    }
+                    return $item;
+                })->prepend(['收货人','收货地址','收货电话'])->toArray();
+                return new Table([], $address);
+            }, '点击查看');
+
+            $grid->disableCreation();
+            $grid->disableRowSelector();
+            $grid->disableActions();
+            
+        });
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        return Admin::form(User::class, function (Form $form) {
+
+            $form->display('id', 'ID');
+
+            $form->display('created_at', 'Created At');
+            $form->display('updated_at', 'Updated At');
+        });
+    }
+}
