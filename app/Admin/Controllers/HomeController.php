cacabd8d9fe2dab783eed71364e500f4d2f37b4c
yankai <yankai>
2017-11-13 10:25:37 +0800

    修改

--- app/Admin/Controllers/HomeController.php
+++ app/Admin/Controllers/HomeController.php
@@ -3,7 +3,12 @@
 namespace App\Admin\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Article;
+use App\Models\Cases;
 use App\Models\Config;
+use App\Models\Order;
+use App\Models\ShopItem;
+use App\User;
 use Encore\Admin\Facades\Admin;
 use Encore\Admin\Layout\Column;
 use Encore\Admin\Layout\Content;
@@ -29,14 +34,19 @@ class HomeController extends Controller
     {
         return Admin::content(function (Content $content) {
 
-            $content->header('Dashboard');
-            $content->description('Description...');
+            $content->header('首页');
+            $content->description('首页');
 
-            $content->row(function ($row) {
-                $row->column(3, new InfoBox('New Users', 'users', 'aqua', '/admin/users', '1024'));
-                $row->column(3, new InfoBox('New Orders', 'shopping-cart', 'green', '/admin/orders', '150%'));
-                $row->column(3, new InfoBox('Articles', 'book', 'yellow', '/admin/articles', '2786'));
-                $row->column(3, new InfoBox('Documents', 'file', 'red', '/admin/files', '698726'));
+            $user_count = User::count();
+            $order_count = Order::count();
+            $article_count = Article::count();
+            $shop_item_count = ShopItem::count();
+
+            $content->row(function ($row) use($user_count,$order_count,$article_count,$shop_item_count) {
+                $row->column(3, new InfoBox('会员数', 'users', 'aqua', '/admin/user', $user_count));
+                $row->column(3, new InfoBox('订单数', 'shopping-cart', 'green', '/admin/order', $order_count));
+                $row->column(3, new InfoBox('文章数', 'book', 'yellow', '/admin/article', $article_count));
+                $row->column(3, new InfoBox('产品数', 'file', 'red', '/admin/shopitem', $shop_item_count));
             });
 
             $content->row(function (Row $row) {
