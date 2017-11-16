bb1865d3b4100be4b6df6a61dfcfb09a87f2a617
yankai <yankai>
2017-11-16 20:01:36 +0800

    123

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -62,7 +62,8 @@ class HomeController extends Controller
         }
         if($catalog->id == 20 || $catalog->parent_id == 20){
             $catalog = Catalog::find(20);
-            $list = ShopItem::where('catalog_id',$catalog_id)->orderByDesc('sort')->orderByDesc('created_at')->paginate(9);
+//            $list = ShopItem::where('catalog_id',$catalog_id)->orderByDesc('sort')->orderByDesc('created_at')->paginate(9);
+            $list = $catalog->subCata->shopitem()->orderByDesc('sort')->orderByDesc('created_at')->paginate(9):
             return view('product_list',[
                 'banners' => Article::where('catalog_id',37)->where('is_display',1)->orderBy('sort','desc')
                     ->orderBy('created_at','desc')->get(),
@@ -70,7 +71,7 @@ class HomeController extends Controller
                 'catalog' => $catalog,
                 'catalog_set' => $catalog_set,
                 'top_catalog' => $top_catalog,
-                'set_nev' => Catalog::$set_nev[$top_catalog_id],
+                'set_nev' => Catalog::$set_nev[$catalog->parent_id],
                 'three_catalog' => $catalog->subCata,
                 'three_catalog_id' => $catalog_id
             ]);
